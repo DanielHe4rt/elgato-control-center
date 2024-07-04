848e4a4feed5301a676dda380d602a2bb68e67c0
Daniel Boll <danielboll.academico@gmail.com>
2024-07-04 18:05:02 -0300

    feat(Cargo.toml): add colog crate dependency for logging feat(rustfmt.toml): create rustfmt configuration file with version, tab spaces, and edition settings refactor(client.rs): improve code formatting and indentation for ElgatoClient struct and methods refactor(components.rs): refactor create_temperature_scale and create_brightness_scale functions for better readability refactor(components.rs): refactor toggle_button function for better readability refactor(devices.rs): improve code formatting and indentation for Light, LightStrip, Keylight structs refactor(devices.rs): improve code formatting and indentation for BaseLight and KeylightTrait traits
    refactor(main.rs): reorganize imports and remove unused imports to clean up the code
    feat(main.rs): add logging statements using the log crate to improve debugging
    feat(network.rs): add debug logging for network scanning and device discovery to aid in troubleshooting
    
    Signed-off-by: Daniel Boll <danielboll.academico@gmail.com>

--- src/client.rs
+++ src/client.rs
@@ -2,73 +2,74 @@ use crate::devices::{Light, LightBucket};
 
 #[derive(Clone, Debug)]
 pub struct ElgatoClient {
-    endpoint: String,
-    client: reqwest::blocking::Client,
-    pub light: Light,
+  endpoint: String,
+  client: reqwest::blocking::Client,
+  pub light: Light,
 }
 
-
 impl ElgatoClient {
-    pub fn new(ip: &str) -> Self {
-        let client = reqwest::blocking::Client::new();
-        let endpoint = format!("http://{}:9123{}", ip, "/elgato/lights");
+  pub fn new(ip: &str) -> Self {
+    let client = reqwest::blocking::Client::new();
+    let endpoint = format!("http://{ip}:9123/elgato/lights");
 
-        let response = client.get(endpoint.clone()).send().unwrap();
-        let light_bucket: LightBucket = response.json().unwrap();
+    let response = client.get(endpoint.clone()).send().unwrap();
+    let light_bucket: LightBucket = response.json().unwrap();
 
-        Self {
-            endpoint,
-            client,
-            light: Light::from(light_bucket.lights[0].clone()),
-        }
+    Self {
+      endpoint,
+      client,
+      light: light_bucket.lights[0].clone(),
+    }
+  }
+
+  pub fn toggle(&mut self) {
+    match &self.light {
+      Light::Keylight(light) => {
+        let mut light = light.clone();
+        light.toggle();
+        self.light = Light::Keylight(light);
+      }
+      Light::LightStrip(light) => {
+        let mut light = light.clone();
+        light.toggle();
+        self.light = Light::LightStrip(light);
+      }
     }
 
-    pub fn toggle(&mut self) {
-        match &self.light {
-            Light::Keylight(light) => {
-                let mut light = light.clone();
-                light.toggle();
-                self.light = Light::Keylight(light);
-            }
-            Light::LightStrip(light) => {
-                let mut light = light.clone();
-                light.toggle();
-                self.light = Light::LightStrip(light);
-            }
-            _ => panic!("Unknown light type")
-        }
+    self
+      .client
+      .put(&self.endpoint)
+      .json(&self.prepare_payload())
+      .send()
+      .unwrap();
+  }
 
-        self.client.put(&self.endpoint)
-            .json(&self.prepare_payload())
-            .send()
-            .unwrap();
+  pub fn set_brightness(&mut self, brightness: u32) {
+    match &self.light {
+      Light::Keylight(light) => {
+        let mut light = light.clone();
+        light.set_brightness(brightness);
+        self.light = Light::Keylight(light);
+      }
+      Light::LightStrip(light) => {
+        let mut light = light.clone();
+        light.set_brightness(brightness);
+        self.light = Light::LightStrip(light);
+      }
     }
 
-    pub fn set_brightness(&mut self, brightness: u32) {
-        match &self.light {
-            Light::Keylight(light) => {
-                let mut light = light.clone();
-                light.set_brightness(brightness);
-                self.light = Light::Keylight(light);
-            }
-            Light::LightStrip(light) => {
-                let mut light = light.clone();
-                light.set_brightness(brightness);
-                self.light = Light::LightStrip(light);
-            }
-            _ => panic!("Unknown light type")
-        }
+    self
+      .client
+      .put(&self.endpoint)
+      .json(&self.prepare_payload())
+      .send()
+      .unwrap();
+  }
 
-        self.client.put(&self.endpoint)
-            .json(&self.prepare_payload())
-            .send()
-            .unwrap();
+  fn prepare_payload(&self) -> LightBucket {
+    LightBucket {
+      number_of_lights: 1,
+      lights: vec![self.light.clone()],
     }
-
-    fn prepare_payload(&self) -> LightBucket {
-        LightBucket {
-            number_of_lights: 1,
-            lights: vec![self.light.clone()],
-        }
-    }
+  }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,112 +1,99 @@
 use std::cell::RefCell;
 use std::rc::Rc;
-use std::str::FromStr;
 
-use gtk::{Application, ApplicationWindow, glib};
-use gtk4 as gtk;
-use gtk4::{ColorChooser, ColorChooserDialog, ColorChooserWidget, Label, Orientation};
-use gtk4::builders::ColorChooserWidgetBuilder;
-use gtk4::cairo::{Gradient, RadialGradient};
 use gtk::prelude::*;
-use serde::{Deserialize, Serialize};
+use gtk::{glib, Application, ApplicationWindow};
+use gtk4 as gtk;
+use gtk4::{Label, Orientation};
+use log::debug;
 
 mod client;
-mod network;
-mod devices;
 mod components;
+mod devices;
+mod network;
 
 fn main() -> glib::ExitCode {
-    let application = Application::builder()
-        .application_id("com.danielheart.elpobre")
-        .build();
+  colog::init();
+  debug!("Starting application");
 
-    application.connect_activate(application_interface());
+  let application = Application::builder()
+    .application_id("com.danielheart.elpobre")
+    .build();
 
-    application.run()
+  application.connect_activate(application_interface);
+
+  application.run()
 }
 
+fn application_interface(app: &Application) {
+  let container = gtk::Box::new(Orientation::Vertical, 10);
+  let window = ApplicationWindow::builder()
+    .application(app)
+    .title("Elgato Control Center de pobre")
+    .default_width(400)
+    .default_height(600)
+    .build();
 
-fn application_interface() -> fn(&Application) {
-    |app| {
-        let container = gtk::Box::new(Orientation::Vertical, 10);
-        let window = ApplicationWindow::builder()
-            .application(app)
-            .title("Elgato Control Center de pobre")
-            .default_width(400)
-            .default_height(600)
-            .build();
+  //let devices = get_active_devices();
+  let devices = vec![
+    client::ElgatoClient::new("192.168.1.11"),
+    client::ElgatoClient::new("192.168.1.63"),
+  ];
 
+  for device in devices {
+    debug!("Device: {:?}", device);
+    let grid = gtk::Grid::builder()
+      .margin_start(6)
+      .margin_end(6)
+      .margin_top(6)
+      .margin_bottom(6)
+      .halign(gtk::Align::Start)
+      .valign(gtk::Align::Start)
+      .row_spacing(6)
+      .column_homogeneous(true)
+      .column_spacing(6)
+      .build();
 
-        //let devices = get_active_devices();
-        let devices = vec![
-            client::ElgatoClient::new("192.168.1.11"),
-            client::ElgatoClient::new("192.168.1.63"),
-        ];
+    match &device.light {
+      devices::Light::Keylight(light) => {
+        debug!("Lightstrip: {:?}", light);
+        let client = Rc::new(RefCell::new(device));
 
-        for device in devices {
-            println!("Device: {:?}", device);
-            let grid = gtk::Grid::builder()
-                .margin_start(6)
-                .margin_end(6)
-                .margin_top(6)
-                .margin_bottom(6)
-                .halign(gtk::Align::Start)
-                .valign(gtk::Align::Start)
-                .row_spacing(6)
-                .column_homogeneous(true)
-                .column_spacing(6)
-                .build();
-            match &device.light {
-                devices::Light::Keylight(light) => {
-                    println!("Lightstrip: {:?}", light);
-                    let client = Rc::new(RefCell::new(device));
+        let button = components::toggle_button(client.clone());
 
+        let brightness_box = gtk::Box::new(Orientation::Horizontal, 100);
+        brightness_box.append(&components::create_brightness_scale(client.clone()));
 
-                    let button = components::toggle_button(client.clone());
+        let temperature_box = gtk::Box::new(Orientation::Horizontal, 100);
+        temperature_box.append(&components::create_temperature_scale(client.clone()));
 
-                    let brightness_box = gtk::Box::new(Orientation::Horizontal, 100);
-                    brightness_box.append(&components::create_brightness_scale(client.clone()));
+        grid.attach(&button, 0, 1, 1, 4);
+        grid.attach(&brightness_box, 2, 2, 2, 1);
+        grid.attach(&temperature_box, 2, 3, 2, 1);
+      }
+      devices::Light::LightStrip(light) => {
+        debug!("Lightstrip: {:?}", light);
+        let client = Rc::new(RefCell::new(device));
 
-                    let temperature_box = gtk::Box::new(Orientation::Horizontal, 100);
-                    temperature_box.append(&components::create_temperature_scale(client.clone()));
+        let button = components::toggle_button(client.clone());
 
-                    grid.attach(&button, 0, 1, 1, 4);
-                    grid.attach(&brightness_box, 2, 2, 2, 1);
-                    grid.attach(&temperature_box, 2, 3, 2, 1);
-                }
-                devices::Light::LightStrip(light) => {
-                    println!("Lightstrip: {:?}", light);
-                    let client = Rc::new(RefCell::new(device));
+        let brightness_box = gtk::Box::new(Orientation::Horizontal, 100);
+        brightness_box.append(&components::create_brightness_scale(client.clone()));
 
-                    let button = components::toggle_button(client.clone());
-
-                    let brightness_box = gtk::Box::new(Orientation::Horizontal, 100);
-                    brightness_box.append(&components::create_brightness_scale(client.clone()));
-
-
-                    grid.attach(&button, 0, 1, 1, 4);
-                    grid.attach(&brightness_box, 2, 2, 2, 1);
-
-                }
-            }
-            container.append(&grid);
-        }
-
-        let mut title = Label::default();
-
-
-        // Create a new scale (volume bar)
-
-        // Add the scale to the vertical box
-        title.set_text("Elgato Control Pobre");
-
-
-
-        window.set_child(Some(&container));
-        window.present();
+        grid.attach(&button, 0, 1, 1, 4);
+        grid.attach(&brightness_box, 2, 2, 2, 1);
+      }
     }
+    container.append(&grid);
+  }
+
+  let title = Label::default();
+
+  // Create a new scale (volume bar)
+
+  // Add the scale to the vertical box
+  title.set_text("Elgato Control Pobre");
+
+  window.set_child(Some(&container));
+  window.present();
 }
-
-
-
-
